d1d9e3a30d8a679ff5910a6c9b15d3205ecc571a
Zhikai Xing <haoyunbaobei1209@gmail.com>
2024-05-25 15:40:06 +0800

    Update ans
    pass 10 unit test
    do not implement brew

--- include/espresso_based.h
+++ include/espresso_based.h
@@ -1,4 +1,25 @@
 #ifndef ESPRESSO_BASED_H
 #define ESPRESSO_BASED_H
 
-#endif // ESPRESSO_BASED_H
+#include <string>
+#include <vector>
+
+#include "ingredient.h"
+class EspressoBased {
+ public:
+  virtual std::string get_name() const = 0;
+  virtual double price() const = 0;
+
+  void brew() const;
+  std::vector<Ingredient*>& get_ingredients();
+  virtual ~EspressoBased();
+
+ protected:
+  EspressoBased();
+  EspressoBased(const EspressoBased& esp);
+  void operator=(const EspressoBased& esp);
+
+  std::vector<Ingredient*> ingredients;
+  std::string name;
+};
+#endif  // ESPRESSO_BASED_H
