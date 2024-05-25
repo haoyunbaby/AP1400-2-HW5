7f4baf20da87d372123ea3215724756a9d02790a
Zhikai Xing <haoyunbaobei1209@gmail.com>
2024-05-25 15:49:40 +0800

    Update ans
    pass 10 unit test
    do not implement brew

--- src/espresso_based.cpp
+++ src/espresso_based.cpp
@@ -1,1 +1,27 @@
 #include "espresso_based.h"
+using namespace std;
+
+vector<Ingredient*>& EspressoBased::get_ingredients() { return ingredients; }
+EspressoBased::~EspressoBased() {
+  for (const auto& i : ingredients) delete i;
+  ingredients.clear();
+}
+EspressoBased::EspressoBased() {}
+EspressoBased::EspressoBased(const EspressoBased& esp) : name(esp.name) {
+  ingredients.reserve(esp.ingredients.size());
+  for (const auto& ing : esp.ingredients) {
+    ingredients.push_back(ing->clone());
+  }
+}
+
+void EspressoBased::operator=(const EspressoBased& esp) {
+  if (this == &esp) {
+    return;
+  }
+  for (const auto& i : ingredients) delete i;
+  ingredients.clear();
+  name = esp.name;
+  for (const auto& ing : esp.ingredients) {
+    ingredients.push_back(ing->clone());
+  }
+}
