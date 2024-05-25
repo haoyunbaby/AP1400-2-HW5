70e0ac561df7a7587da3bc46e1b8ff9f209a88c1
Zhikai Xing <haoyunbaobei1209@gmail.com>
2024-05-25 15:41:48 +0800

    Update ans
    pass 10 unit test

--- include/mocha.h
+++ include/mocha.h
@@ -1,4 +1,24 @@
 #ifndef MOCHA_H
 #define MOCHA_H
+#include <string>
+#include <vector>
 
-#endif // MOCHA_H
+#include "espresso_based.h"
+#include "sub_ingredients.h"
+class Mocha : public EspressoBased {
+ public:
+  Mocha();
+  Mocha(const Mocha &moc);
+  virtual ~Mocha() override;
+  void operator=(const Mocha &moc);
+
+  virtual std::string get_name() const override;
+  virtual double price() const override;
+
+  void add_side_item(Ingredient *side);
+  std::vector<Ingredient *> &get_side_items();
+
+ private:
+  std::vector<Ingredient *> side_items;
+};
+#endif  // MOCHA_H
