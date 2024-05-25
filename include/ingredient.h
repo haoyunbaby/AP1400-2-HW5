7f1ab54b5b9a36986dec85d4463bdc818177df11
Zhikai Xing <haoyunbaobei1209@gmail.com>
2024-05-25 15:37:46 +0800

    Update ans
    pass 10 unit test

--- include/ingredient.h
+++ include/ingredient.h
@@ -1,4 +1,20 @@
 #ifndef INGREDIENT_H
 #define INGREDIENT_H
+#include <string>
 
-#endif // INGREDIENT_H
+class Ingredient {
+ public:
+  double get_price_unit() const { return price_unit; }
+  size_t get_units() const { return units; }
+  virtual std::string get_name() const = 0;
+  virtual Ingredient *clone() const = 0;
+  double price() const { return price_unit * units; }
+
+ protected:
+  Ingredient(double price_unit, size_t units)
+      : price_unit(price_unit), units(units) {}
+  double price_unit;
+  size_t units;
+  std::string name;
+};
+#endif  // INGREDIENT_H
