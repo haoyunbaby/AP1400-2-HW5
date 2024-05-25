5b60985770de9a13a2c40dfed09267951b9688ca
Zhikai Xing <haoyunbaobei1209@gmail.com>
2024-05-25 15:41:01 +0800

    Update ans
    pass 10 unit test

--- include/cappuccino.h
+++ include/cappuccino.h
@@ -1,4 +1,25 @@
 #ifndef CAPPUCCINO
 #define CAPPUCCINO
 
-#endif // CAPPUCCINO
+#include <string>
+#include <vector>
+
+#include "espresso_based.h"
+#include "sub_ingredients.h"
+class Cappuccino : public EspressoBased {
+ public:
+  Cappuccino();
+  Cappuccino(const Cappuccino &cap);
+  virtual ~Cappuccino() override;
+  void operator=(const Cappuccino &cap);
+
+  virtual std::string get_name() const override;
+  virtual double price() const override;
+
+  void add_side_item(Ingredient *side);
+  std::vector<Ingredient *> &get_side_items();
+
+ private:
+  std::vector<Ingredient *> side_items;
+};
+#endif  // CAPPUCCINO
